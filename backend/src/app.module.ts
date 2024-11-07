0112074e3ff353372c77eaef91b362c114dfb1bc
Amit Sharma <121919097+amitcodee@users.noreply.github.com>
2024-11-07 15:02:34 +0530

    PostgreSQL

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,28 @@
 /* eslint-disable prettier/prettier */
 // src/app.module.ts
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { ItemsModule } from './items/items.module';
+
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRoot({
+//       type: 'mysql',
+//       host: 'localhost',
+//       port: 3306,
+//       username: 'root', // Default username in XAMPP
+//       password: '', // Default password in XAMPP (usually empty)
+//       database: 'nest',
+//       autoLoadEntities: true,
+//       synchronize: true, // Only for development; disable in production
+//     }),
+//     ItemsModule,
+//   ],
+// })
+// export class AppModule {}
+// src/app.module.ts
+// 
+// postgraph
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsModule } from './items/items.module';
@@ -7,14 +30,14 @@ import { ItemsModule } from './items/items.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root', // Default username in XAMPP
-      password: '', // Default password in XAMPP (usually empty)
-      database: 'nest',
+      type: 'postgres', // Change this to 'postgres' for PostgreSQL
+      host: 'localhost', // Use your PostgreSQL server's host
+      port: 5432, // Default PostgreSQL port
+      username: 'postgres', // Replace with your PostgreSQL username
+      password: '123', // Replace with your PostgreSQL password
+      database: 'nest-db', // Replace with your PostgreSQL database name
       autoLoadEntities: true,
-      synchronize: true, // Only for development; disable in production
+      synchronize: true, // Enable for development only
     }),
     ItemsModule,
   ],
